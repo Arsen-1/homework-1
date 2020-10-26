c419c1d97a0be1650628d25a81283b57227bf52e
Arsen-1 <69711993+Arsen-1@users.noreply.github.com>
2020-10-26 22:06:03 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+bool isPrime(int n){
+   if(n == 1)
+   return false;
+  for(int i = 2; i*i <= n; ++i) {
+      if (n % i == 0) 
+          return false;
+  }
+  return true;
+     
+}
+
+int main(){
+    
+    
+    int size;
+    std::cin >> size;
+    int* dynArr = new int[size];
+
+    for(int i = 0; i < size; ++i){
+        std::cin >> dynArr[i]; 
+        int count = 0; 
+        for(int j = 2; j < dynArr[i]; j++){ 
+           if(isPrime(j)){
+             count ++;
+            }
+        }
+        int* b = new int[i];
+        b[i] = count;
+        std::cout  << b[i] << " " ;
+    }
+    
+ delete[] dynArr;
+ return 0;  
+}       
